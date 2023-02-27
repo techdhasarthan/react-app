b749e24765a2e0ac014c095fc071cf376e0c3808
techdhasarthan <112262869+techdhasarthan@users.noreply.github.com>
2023-02-27 15:15:10 +0000

    Master anime letter changed  to movie

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,37 +4,37 @@ import '../styles/videos.css'
 import {AiFillPlayCircle} from 'react-icons/ai'
 import NoImg from './NoImage.png'
 
-function Anime() {
-  const [AnimeData,setAnimeData] = useState([])
+function Movies() {
+  const [moviesData,setMoviesData] = useState([])
   // const Api = 'https://api.themoviedb.org/3/movie/550?api_key=93fa04b3d14728f50673dbfa930c4d75/discover/movie'
   const Api = "https://api.themoviedb.org/3/discover/movie"
   const Images = 'https://image.tmdb.org/t/p/w500/'
-  const AnimeCall = async () => {
+  const MovieCall = async () => {
     const data = await axios.get(Api,{
       params:{
         api_key: '93fa04b3d14728f50673dbfa930c4d75',
       }
     })
     const results =data.data.results
-    setAnimeData(results)
+    setMoviesData(results)
     //  console.log(data.data.results);
     
   }
 useEffect(() => {
-  AnimeCall()
+  MovieCall()
 },[])
-console.log(AnimeData)
+console.log(moviesData)
   return (
     <Fragment>
       
-      {AnimeData.map((Anime) => {
+      {moviesData.map((movie) => {
         return(
       <Fragment>
       <div id = 'container'>
        
         <AiFillPlayCircle color = 'green' fontSize={40} id="playIcon"/>
-        <img src={Anime.poster_path ? `${Images}${Anime.poster_path}` : NoImg} alt="image not fount 404"/>
-        <h3>{Anime.title}</h3>
+        <img src={movie.poster_path ? `${Images}${movie.poster_path}` : NoImg} alt="image not fount 404"/>
+        <h3>{movie.title}</h3>
       </div>
     </Fragment>
        )
@@ -46,4 +46,4 @@ console.log(AnimeData)
   )
 }
 
-export default Anime
+export default Movies
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react'
 import{HiSearch} from 'react-icons/hi'
 import { Routes,Route, NavLink } from 'react-router-dom'
-import Anime from './Anime'
+import Movies from './Movies'
 import Tv_Shows from './Tv_Shows'
 import Trending from './Trends'
 import Pricing from './Pricing'
@@ -13,23 +13,23 @@ function NavBar() {
     <Fragment>
         <nav className={toggle ? '' :'NavBarColor'}>
             <div className='nav_options'>
-              <h1 id = {toggle ? '' :'heading'}> Akastsuki</h1>
-                <NavLink to="/Anime" style={({isActive}) => {return {color:isActive ? "#fff" : '#0ed6e8'}}}>
-                <span id = {toggle ? 'Animes' :'AnimeLight'}>Anime</span>
+              <h1 id = {toggle ? '' :'heading'}>MovieInfo</h1>
+                <NavLink to="/Movies" style={({isActive}) => {return {color:isActive ? "#fff" : '#0ed6e8'}}}>
+                <span id = {toggle ? 'Moviess' :'MoviesLight'}>Movies</span>
                 </NavLink>
                
                 <NavLink to="/Tv_Shows" style={({isActive}) => {return {color:isActive ? "#fff" : '#0ed6e8'}}}>
-                <span id = {toggle ? 'Anime' :'AnimeLight'}>Tv_Shows</span>
+                <span id = {toggle ? 'Movies' :'MoviesLight'}>Tv_Shows</span>
                 </NavLink>
                 <NavLink to="/Trending" style={({isActive}) => {return {color:isActive ? "#fff" : '#0ed6e8'}}}>
-                <span id = {toggle ? 'Anime' :'AnimeLight'}>Trending</span>
+                <span id = {toggle ? 'Movies' :'MoviesLight'}>Trending</span>
                 </NavLink>
                 <NavLink to="/Pricing" style={({isActive}) => {return {color:isActive ? "#fff" : '#0ed6e8'}}}>
-                <span id = {toggle ? 'Anime' :'AnimeLight'}>Pricing</span>
+                <span id = {toggle ? 'Movies' :'MoviesLight'}>Pricing</span>
                 </NavLink>
             </div>
             <div className='input-group'>
-            <input type="text" placeholder='Search your fav Anime'/>
+            <input type="text" placeholder='Search your fav movies'/>
 
             <HiSearch fontSize={21} color="black" id='search'/>
             <div id="Color-switcher" onClick={() => setToggle(!toggle)}>
@@ -38,7 +38,7 @@ function NavBar() {
             </div>
         </nav>
         <Routes>
-          <Route path='Anime' element={<Anime /> }/>
+          <Route path='Movies' element={<Movies /> }/>
           <Route path='Tv_Shows' element={<Tv_Shows /> }/>
           <Route path='Trending' element={<Trending /> }/>
           <Route path='Pricing' element={<Pricing /> }/>
